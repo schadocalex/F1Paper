533bb4e31129f324cf2deb84ea721d0cd0daca0f
Alexis <schadoc_alex@hotmail.fr>
2015-12-12 08:20:42 +0100

    init phaser lib

---
+++ src/main.js
@@ -0,0 +1,17 @@
+var game = new Phaser.Game("100", "100", Phaser.AUTO, '', {
+    preload: preload,
+    create: create,
+    update: update
+});
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+    
+}
